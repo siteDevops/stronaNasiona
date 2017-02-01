913831b7366854096ca2fe5059ae460ca1095e1d
Dawid Jeż <dawidjez@gmail.com>
2017-02-01 12:40:03 +0100

    dodawanie produktu (automatyczna zmiana nazwy pliku)

--- application/modules/admin/controllers/ProductsController.php
+++ application/modules/admin/controllers/ProductsController.php
@@ -1,10 +1,13 @@
 <?php
 require_once 'AbstractAdminController.php';
+
 class Admin_ProductsController extends AbstractAdminController
 {
-    public function indexAction() {
+    public function indexAction()
+    {
         $this->forward('list');
     }
+
     public function listAction()
     {
         $this->view->products = Application_Model_Products::getList();
@@ -14,22 +17,37 @@ class Admin_ProductsController extends AbstractAdminController
     {
 
     }
+
     public function addAction()
     {
-        if(!$this->getRequest()->isPost()){
+        if (!$this->getRequest()->isPost()) {
             return;
         }
         $adapter = new Zend_File_Transfer_Adapter_Http();
-        $name = $adapter->getFileName();
-        while(file_exists(UPLOAD_IMAGES_PATH.'/'.$name)){
-            $name = $adapter->set();
-        }
+
+
         $adapter->setDestination(UPLOAD_IMAGES_PATH);
 
+        $name = md5($adapter->getFileName());
+
+        while(file_exists(UPLOAD_IMAGES_PATH.'/'.$name)){
+            $name = md5($name);
+        }
+
+        $adapter->addFilter('Rename', array('target' => UPLOAD_IMAGES_PATH . '/' . $name, 'overwrite' => true));
         if (!$adapter->receive()) {
             $messages = $adapter->getMessages();
             echo implode("\n", $messages);
         }
+    }
+
+    protected function _findexts($filename)
+    {
+        $filename = strtolower($filename);
+        $exts = explode(".", $filename);
+        $n = count($exts) - 1;
+        $exts = $exts[$n];
+        return $exts;
     }
 
 
